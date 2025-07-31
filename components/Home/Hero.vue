c3465f09c06556259542ef22ea2a0d81405e55de
Skrauz <krauzowiczs42@gmail.com>
2025-07-31 17:15:03 +0200

    RWD tweaks

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -1,31 +1,15 @@
 <script setup lang="ts"></script>
 <template>
   <div
-    class="h-[calc(100dvh-138px)] relative flex flex-col justify-center pb-24"
+    class="h-[calc(100dvh-96px)] md:h-[calc(100dvh-138px)] relative flex flex-col justify-center pb-20 md:pb-24"
   >
     <div class="arrow-flex w-full absolute bottom-0 left-0 flex justify-center">
-      <button class="p-4 mb-7">
+      <button class="p-4 mb-2 md:mb-7">
         <nuxt-img class="w-5" src="/img/hero-arrow.svg"></nuxt-img>
       </button>
     </div>
 
     <div class="relative">
-      <div class="social-links absolute left-8 -mt-1">
-        <div class="flex flex-col items-center gap-2">
-            <nuxt-link class="cursor-pointer opacity-70 transition-all hover:opacity-100" to="https://www.linkedin.com/in/skrauz/" target="_blank">
-              <nuxt-img class="w-8" src="/img/socials/linkedin.svg"></nuxt-img>
-            </nuxt-link>
-            <nuxt-link class="cursor-pointer opacity-70 transition-all hover:opacity-100" to="https://github.com/Skrauz" target="_blank">
-              <nuxt-img class="w-8" src="/img/socials/github.svg"></nuxt-img>
-            </nuxt-link>
-            <nuxt-link class="cursor-pointer opacity-70 transition-all hover:opacity-100" to="https://www.instagram.com/skrauz_dev/" target="_blank">
-              <nuxt-img class="w-8" src="/img/socials/instagram.svg"></nuxt-img>
-            </nuxt-link>
-            <nuxt-link class="cursor-pointer opacity-70 transition-all hover:opacity-100" to="mailto:krauzowiczs42@gmail.com" target="_blank">
-              <nuxt-img class="w-8" src="/img/socials/mail.svg"></nuxt-img>
-            </nuxt-link>
-        </div>
-      </div>
 
       <div class="common-container">
         <div class="hero flex flex-col items-center">
@@ -50,6 +34,23 @@
             </p>
             <nuxt-img class="w-6" src="/img/contact-arrow.svg"></nuxt-img>
           </nuxt-link>
+        </div>
+      </div>
+
+      <div class="social-links absolute left-8 -mt-3 hidden md:block top-0">
+        <div class="flex flex-col items-center gap-3">
+            <nuxt-link class="cursor-pointer opacity-70 transition-all hover:opacity-100" to="https://www.linkedin.com/in/skrauz/" target="_blank">
+              <nuxt-img class="w-8" src="/img/socials/linkedin.svg"></nuxt-img>
+            </nuxt-link>
+            <nuxt-link class="cursor-pointer opacity-70 transition-all hover:opacity-100" to="https://github.com/Skrauz" target="_blank">
+              <nuxt-img class="w-8" src="/img/socials/github.svg"></nuxt-img>
+            </nuxt-link>
+            <nuxt-link class="cursor-pointer opacity-70 transition-all hover:opacity-100" to="https://www.instagram.com/skrauz_dev/" target="_blank">
+              <nuxt-img class="w-8" src="/img/socials/instagram.svg"></nuxt-img>
+            </nuxt-link>
+            <nuxt-link class="cursor-pointer opacity-70 transition-all hover:opacity-100" to="mailto:krauzowiczs42@gmail.com" target="_blank">
+              <nuxt-img class="w-8" src="/img/socials/mail.svg"></nuxt-img>
+            </nuxt-link>
         </div>
       </div>
     </div>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,77 +1,171 @@
 <script setup lang="ts">
-
 const isMobileMenuOpen = ref(false);
 const toggleMobileMenu = () => {
   isMobileMenuOpen.value = !isMobileMenuOpen.value;
 };
-
 </script>
 <template>
-  <nav class="common-container w-full flex md:items-end items-center justify-between md:py-12 py-6">
+  <nav
+    class="common-container w-full flex md:items-end items-center justify-between md:py-12 py-6"
+  >
     <!-- Logo -->
     <NuxtLink class="md:mb-1" to="/">
       <NuxtImg class="w-[180px] lg:w-[200px]" src="/img/logo.png" />
     </NuxtLink>
     <!-- Desktop Nav -->
     <div class="items-center gap-8 hidden md:flex">
-      <NuxtLink to="/doswiadczenie"
-        class="text-xl font-bold tracking-tighter relative group nav-link-underline transition-colors hover:text-theme-light">
-        <span class="text-theme-accent">/</span>Doświadczenie <span class="nav-underline"></span>
+      <NuxtLink
+        to="/doswiadczenie"
+        class="text-xl font-bold tracking-tighter relative group nav-link-underline transition-colors hover:text-theme-light"
+      >
+        <span class="text-theme-accent">/</span>Doświadczenie
+        <span class="nav-underline"></span>
       </NuxtLink>
-      <NuxtLink to="/projekty"
-        class="text-xl font-bold tracking-tighter relative group nav-link-underline transition-colors hover:text-theme-light">
-        <span class="text-theme-accent">/</span>Projekty <span class="nav-underline"></span>
+      <NuxtLink
+        to="/projekty"
+        class="text-xl font-bold tracking-tighter relative group nav-link-underline transition-colors hover:text-theme-light"
+      >
+        <span class="text-theme-accent">/</span>Projekty
+        <span class="nav-underline"></span>
       </NuxtLink>
-      <NuxtLink to="/kontakt"
-        class="text-xl font-bold tracking-tighter relative group nav-link-underline transition-colors hover:text-theme-light">
-        <span class="text-theme-accent">/</span>Kontakt <span class="nav-underline"></span>
+      <NuxtLink
+        to="/kontakt"
+        class="text-xl font-bold tracking-tighter relative group nav-link-underline transition-colors hover:text-theme-light"
+      >
+        <span class="text-theme-accent">/</span>Kontakt
+        <span class="nav-underline"></span>
       </NuxtLink>
     </div>
     <!-- Mobile Hamburger Button -->
-    <button @click="toggleMobileMenu" class="md:hidden p-2 cursor-pointer transition-colors hover:text-theme-light"
-      aria-label="Toggle Menu">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-text-dark-1" fill="none" viewBox="0 0 24 24"
-        stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+    <button
+      @click="toggleMobileMenu"
+      class="md:hidden p-2 cursor-pointer transition-colors hover:text-theme-light"
+      aria-label="Toggle Menu"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-8 w-8 text-text-dark-1"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16M4 18h16"
+        />
       </svg>
     </button>
   </nav>
   <!-- Mobile Menu Overlay -->
-  <Transition enter-from-class="opacity-0" enter-active-class="transition-all duration-150 ease-in"
-    enter-to-class="opacity-100" leave-from-class="opacity-100"
-    leave-active-class="transition-all duration-150 ease-out" leave-to-class="opacity-0">
-    <div v-if="isMobileMenuOpen" @click="toggleMobileMenu"
-      class="mobile-menu-backdrop absolute top-0 left-0 w-full h-screen bg-black/50 z-[45]"></div>
+  <Transition
+    enter-from-class="opacity-0"
+    enter-active-class="transition-all duration-150 ease-in"
+    enter-to-class="opacity-100"
+    leave-from-class="opacity-100"
+    leave-active-class="transition-all duration-150 ease-out"
+    leave-to-class="opacity-0"
+  >
+    <div
+      v-if="isMobileMenuOpen"
+      @click="toggleMobileMenu"
+      class="mobile-menu-backdrop fixed top-0 left-0 w-full h-screen bg-black/50 z-[45]"
+    ></div>
   </Transition>
-  <Transition name="fade" enter-active-class="duration-150 ease-out" enter-from-class="opacity-0 -translate-x-36"
-    enter-to-class="opacity-100 translate-x-0" leave-active-class="transition-all duration-150 ease-in"
-    leave-from-class="opacity-100 translate-x-0" leave-to-class="opacity-0 -translate-x-36">
-    <nav v-if="isMobileMenuOpen" class="mobile-menu absolute top-0 left-0 w-[75%] h-screen bg-theme-dark-2 z-50">
+  <Transition
+    name="fade"
+    enter-active-class="duration-150 ease-out"
+    enter-from-class="opacity-0 -translate-x-36"
+    enter-to-class="opacity-100 translate-x-0"
+    leave-active-class="transition-all duration-150 ease-in"
+    leave-from-class="opacity-100 translate-x-0"
+    leave-to-class="opacity-0 -translate-x-36"
+  >
+    <nav
+      v-if="isMobileMenuOpen"
+      class="mobile-menu fixed top-0 left-0 w-[75%] h-screen bg-theme-dark-2 z-50"
+    >
       <div class="ml-2">
-        <div class="common-container flex items-center justify-between md:py-12 py-6">
+        <div
+          class="common-container flex items-center justify-between md:py-12 py-6"
+        >
           <h3 class="text-3xl font-bold tracking-tighter relative group">
             Menu
           </h3>
-          <button @click="toggleMobileMenu" class="p-2 cursor-pointer transition-colors hover:text-theme-light"
-            aria-label="Close Menu"> <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none"
-              viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-            </svg> </button>
+          <button
+            @click="toggleMobileMenu"
+            class="p-2 cursor-pointer transition-colors hover:text-theme-light"
+            aria-label="Close Menu"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-8 w-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
         </div>
         <div class="common-container">
           <div class="flex flex-col gap-4">
-            <NuxtLink to="/doswiadczenie"
-              class="text-xl font-bold tracking-tighter relative group transition-colors hover:text-theme-light">
+            <NuxtLink
+              to="/doswiadczenie"
+              class="text-xl font-bold tracking-tighter relative group transition-colors hover:text-theme-light"
+            >
               <span class="text-theme-accent">/</span>Doświadczenie
             </NuxtLink>
-            <NuxtLink to="/projekty"
-              class="text-xl font-bold tracking-tighter relative group transition-colors hover:text-theme-light">
+            <NuxtLink
+              to="/projekty"
+              class="text-xl font-bold tracking-tighter relative group transition-colors hover:text-theme-light"
+            >
               <span class="text-theme-accent">/</span>Projekty
             </NuxtLink>
-            <NuxtLink to="/kontakt"
-              class="text-xl font-bold tracking-tighter relative group transition-colors hover:text-theme-light">
-              <span class="text-theme-accent">/</span>Kontakt 
+            <NuxtLink
+              to="/kontakt"
+              class="text-xl font-bold tracking-tighter relative group transition-colors hover:text-theme-light"
+            >
+              <span class="text-theme-accent">/</span>Kontakt
             </NuxtLink>
+          </div>
+        </div>
+        <div class="py-6 common-container absolute bottom-0 left-0">
+          <div class="flex items-center gap-4">
+            <nuxt-link
+              class="cursor-pointer"
+              to="https://www.linkedin.com/in/skrauz/"
+              target="_blank"
+            >
+              <nuxt-img class="w-8" src="/img/socials/linkedin.svg"></nuxt-img>
+            </nuxt-link>
+            <nuxt-link
+              class="cursor-pointer"
+              to="https://github.com/Skrauz"
+              target="_blank"
+            >
+              <nuxt-img class="w-8" src="/img/socials/github.svg"></nuxt-img>
+            </nuxt-link>
+            <nuxt-link
+              class="cursor-pointer"
+              to="https://www.instagram.com/skrauz_dev/"
+              target="_blank"
+            >
+              <nuxt-img class="w-8" src="/img/socials/instagram.svg"></nuxt-img>
+            </nuxt-link>
+            <nuxt-link
+              class="cursor-pointer pt-1.5"
+              to="mailto:krauzowiczs42@gmail.com"
+              target="_blank"
+            >
+              <nuxt-img class="w-8" src="/img/socials/mail.svg"></nuxt-img>
+            </nuxt-link>
           </div>
         </div>
       </div>
@@ -94,7 +188,7 @@ const toggleMobileMenu = () => {
   background: var(--color-theme-accent);
   transform: scaleX(0);
   transform-origin: left;
-  transition: transform 0.3s cubic-bezier(.4, 0, .2, 1);
+  transition: transform 0.3s cubic-bezier(0.4, 0, 0.2, 1);
   pointer-events: none;
   z-index: 1;
 }
