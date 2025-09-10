8a199fa65a0e68555bd586ba78a394753f1ed276
Skrauz <krauzowiczs42@gmail.com>
2025-09-10 17:56:08 +0200

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,4 +1,14 @@
 <template>
-  <p>ftr</p>
+  <div class="footer-wrapper relative">
+    <div class="absolute w-full bottom-0 left-0 z-50">
+      <div class="common-container">
+        <div class="flex justify-center py-3">
+          <p class="text-sm text-theme-accent">© {{ year }} Sebastian Krauzowicz</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const year = new Date().getFullYear();
+</script>
