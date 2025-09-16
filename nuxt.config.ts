1101d7271b5018fb904fe6860fe3a8d6e6b7f2d6
Skrauz <krauzowiczs42@gmail.com>
2025-09-16 15:57:33 +0200

    favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,22 +15,46 @@ export default defineNuxtConfig({
   },
 
   site: {
-      url: 'https://skrauz.dev',
-      name: 'Sebastian Krauzowicz - Web Developer',
-      description: 'Strona internetowa i portfolio Sebastiana Krauzowicza',
-      keywords: ['web developer', 'frontend developer', 'portfolio', 'poland'],
-      author: 'Sebastian Krauzowicz',
-      publisher: 'Sebastian Krauzowicz',
-      generator: 'Nuxt',
-      robots: 'index, follow',
-      canonical: 'https://skrauz.dev',
-      og: {
-        type: 'website',
-        title: 'Sebastian Krauzowicz - Web Developer',
-        description: 'Strona internetowa i portfolio Sebastiana Krauzowicza',
-        url: 'https://skrauz.dev',
-        image: '/img/og-image.png',
-      },
+    url: "https://skrauz.dev",
+    name: "Sebastian Krauzowicz - Web Developer",
+    description: "Strona internetowa i portfolio Sebastiana Krauzowicza",
+    keywords: ["web developer", "frontend developer", "portfolio", "poland"],
+    author: "Sebastian Krauzowicz",
+    publisher: "Sebastian Krauzowicz",
+    generator: "Nuxt",
+    robots: "index, follow",
+    canonical: "https://skrauz.dev",
+    og: {
+      type: "website",
+      title: "Sebastian Krauzowicz - Web Developer",
+      description: "Strona internetowa i portfolio Sebastiana Krauzowicza",
+      url: "https://skrauz.dev",
+      image: "/img/og-image.png",
     },
+  },
 
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+
+        { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
+
+        { rel: "manifest", href: "/site.webmanifest" },
+      ],
+    },
+  },
 });
